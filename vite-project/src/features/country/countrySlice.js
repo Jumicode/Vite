71ba3258d0041363ca81c529ca3475fe4774247c
Julio Romero <105522237+Jumicode@users.noreply.github.com>
2023-09-17 14:50:10 +0000

    Redux toolkit configuration

---
+++ vite-project/src/features/country/countrySlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+
+export const countrySlice = createSlice({
+    name:'country',
+
+    initialState:{
+      country:[],
+    },
+
+
+  reducers:{
+    setCountry:(state, action) =>{
+      state.country = action.payload;
+    }
+  }
+   
+})
+
+
+export const {
+  setCountry,
+} = countrySlice.actions;
+
+
+
+
+export default countrySlice.reducer;
